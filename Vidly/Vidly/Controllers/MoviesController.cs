679db26f6c1e773a2f74f29df8dbb3dc1d809045
lahiaouni <lahiaouni.abd@gmail.com>
2017-12-24 17:34:46 +0000

    Custom Routes

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int? movieId)
+        {
+            if (!movieId.HasValue)
+                movieId = 1;
+            return Content("Id =" + movieId);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
